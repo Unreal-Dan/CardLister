2197096b8c4bba237080713cdcd88893e1648f13
Dan <UnrealDanMail@gmail.com>
2025-02-24 03:16:03 -0800

    minor fix

--- php/ebay_create_listing.php
+++ php/ebay_create_listing.php
@@ -45,7 +45,7 @@ $xmlBody = '<?xml version="1.0" encoding="utf-8"?>
     <PrimaryCategory>
       <CategoryID>183454</CategoryID>
     </PrimaryCategory>
-    <StartPrice currencyID="USD">' . number_format((float)$data['startPrice'], 2, '.', '') . '</StartPrice>
+    <StartPrice currencyID="USD">' . sprintf("%.2f", (float)$data['startPrice']) . '</StartPrice>
     <ListingDuration><![CDATA[GTC]]></ListingDuration>
     <ListingType>FixedPriceItem</ListingType>
     <Quantity>1</Quantity>
@@ -56,6 +56,7 @@ $xmlBody = '<?xml version="1.0" encoding="utf-8"?>
       <ShippingServiceOptions>
         <ShippingService>CA_ExpeditedParcel</ShippingService>
         <ShippingServiceCost currencyID="CAD">10.00</ShippingServiceCost>
+        <ShippingServicePriority>1</ShippingServicePriority>
       </ShippingServiceOptions>
     </ShippingDetails>
     <ReturnPolicy>
